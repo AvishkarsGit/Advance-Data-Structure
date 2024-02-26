51c4c47073fa15fbc083578fcd6d74d0c608b4bd
AvishkarGit <109093669+AvishkarsGit@users.noreply.github.com>
2024-02-26 13:58:43 +0000

    seperate chaining

--- pract_02.cpp
+++ pract_02.cpp
@@ -1,23 +1,25 @@
 //pract 02
 #include<iostream>
 #include<stdlib.h>
+#include<string.h>
 using namespace std;
 #define table_size 10
 
 
 typedef struct node{
     int key;
-    char value[256];
+    string value;
     struct node *next;
 }node;
 
 node *hashTable[table_size];
 
-node *create(int key, char value[]){
+node *create(int key, string name){
     node *head;
-    head =(node)malloc(sizeof(node*));
+   
+    head = (node*)malloc(sizeof(node));
     head->key = key;
-    head->value = value;
+    head->value = name;
     head->next = NULL;
     return(head);
 }
@@ -25,7 +27,7 @@ int getBucketIndex(int key){
     int index = key % table_size;
     return index;
 }
-void insert(int key,char value[]){
+void insert(int key,string value){
     node *head;
     head = create(key,value);
     int bucketIndex = getBucketIndex(key);
@@ -39,16 +41,72 @@ void insert(int key,char value[]){
         }
         temp->next = head;
     }
+    cout<<"\nInserted...";
 }
 void print(){
-    for (int i=0;i<table_size;i++){
-        node *p = hashTable[i];
-        cout<<hashTable[i]<<"->";
-        while(p){
-            cout<<p->value<<"->";
+    node *n1;
+    for (int i = 0; i < table_size; i++)
+    {
+        cout<<"\n["<<i<<"]\t";
+        if (hashTable[i]==NULL){
+            continue;
         }
-        cout<<"NULL\n";
+        else{
+            for (n1 = hashTable[i]; n1!=NULL;n1=n1->next)
+            {
+                cout<<"["<<n1->key<<"|"<<n1->value<<"]->";            
+            }
+            
+        }
+        
     }
+}
+void find(int key){
+    int index;
+    node *n1;
+    index = getBucketIndex(key);
+    if (hashTable[index]==NULL){
+        cout<<"\nElement not found!";
+    }
+    else {
+        for (n1 = hashTable[index];n1!=NULL;n1=n1->next)
+        {
+            if (n1->key == key)
+            {
+                cout<<"\nelement is found at index "<<index;
+                break;
+            }
+        }
+        if (n1==NULL)
+        {
+            cout<<"\nElement is not found!!";
+        }   
+    }
+}
+void remove(int key){
+    
+    node *n,*n1;
+    int index;
+    index = getBucketIndex(key);
+    if (hashTable[index]->key == key)
+    {
+        n = hashTable[index];
+        hashTable[index] = hashTable[index]->next;
+        free(n);
+        return;
+    }
+    for (n = hashTable[index]; n->next!=NULL;n=n->next)
+    {
+        n1 = n->next;
+        if (n1->key,key)
+        {
+            n->next = n1->next;
+            free(n1);
+            break;
+        }
+    }
+    
+    
 }
 void initialize()
 {
@@ -60,34 +118,48 @@ int main(){
 
     initialize();
     int choice;
+    int key;
+    string value;
 
     do {
 
-        cout<<"*** Dictionary operations ***";
+        cout<<"\n\n*** Dictionary operations ***";
         cout<<"\n1.Insert";
-        cout<<"\n2.Find";
-        cout<<"\n3.Delete";
-        cout<<"\n4.Exit";
+        cout<<"\n2.Print";
+        cout<<"\n3.Find";
+        cout<<"\n4.Delete";
+        cout<<"\n5.Exit";
         cout<<"\nEnter your choice:";
         cin>>choice;
         switch(choice){
 
             case 1: 
-                insert(45,"ganesh");       
+                cout<<"\nEnter key:";
+                cin>>key;
+                cout<<"\nEnter value:";
+                cin>>value;
+                insert(key,value);
                 break;
 
             case 2: 
                 print();
                 break;
             case 3: 
+                cout<<"\nEnter key to find: ";
+                cin>>key;
+                find(key);
                 break;
             case 4: 
+                cout<<"\nEnter key to find: ";
+                cin>>key;
+                remove(key);
                 break;
-
+            case 5: 
+                break;
             default:
                 cout<<"\nInvalid choice";
             break;
         }
-    }while(choice!=4);
+    }while(choice!=5);
     return 0;
 }
