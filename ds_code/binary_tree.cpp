345550241efd8db44e978fbc9dba6cd0dc79d7dd
AvishkarGit <109093669+AvishkarsGit@users.noreply.github.com>
2024-02-28 07:31:17 +0000

    binary tree operations

---
+++ ds_code/binary_tree.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+using namespace std;
+
+typedef struct node{
+    int data;
+    struct node *left;
+    struct node *right;
+}node;
+
+node *create(int data){
+    node *n;
+    n = (node*)malloc(sizeof(node));
+    n->data = data;
+    n->left = NULL;
+    n->right = NULL;
+    return(n);
+}
+void print(node *root){
+    if (root!=NULL)
+    {
+        cout<<root->data<<"->";
+        print(root->left);
+        print(root->right);
+    }
+}
+
+void preOrderTraversal(node *root){
+    if (root!=NULL)
+    {
+        cout<<root->data<<"->";
+        preOrderTraversal(root->left);
+        preOrderTraversal(root->right);
+
+    }
+}
+void postOrderTraversal(){
+
+}
+
+int main()
+{
+    /*
+           10
+          /  \
+        20    30
+       / \   /  \
+      40 50 60  70
+    */
+    node *n1 = create(10);
+    node *n2 = create(20);
+    node *n3 = create(30);
+    node *n4 = create(40);
+    node *n5 = create(50);
+    node *n6 = create(60);
+    node *n7 = create(70);
+    
+    // link
+    n1->left = n2;
+    n1->right = n3;
+    n2->left = n4;
+    n2->right = n5;
+    n3->left = n6;
+    n3->right = n7;
+
+    cout<<"\n*** Pre-Order Traversal ***\n";
+    preOrderTraversal(n1);
+    cout<<"\n";
+    return 0;
+}
