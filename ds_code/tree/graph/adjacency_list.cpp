5ab6c703821a7b05bb3cf079a5796817c1df7cac
AvishkarGit <109093669+AvishkarsGit@users.noreply.github.com>
2024-04-27 16:29:47 +0000

    adjacency list

---
+++ ds_code/tree/graph/adjacency_list.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Edge
+{
+    int src,dest;
+    public:
+        Edge(int s,int d)
+        {
+            this->src = s;
+            this->dest = d;
+        }
+};
+int main(){
+    int v = 4;
+
+    vector<Edge> graph;
+    return 0;
+}
