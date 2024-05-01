62aae7679300dac4cda0066bb1a5b85cd2dd0919
AvishkarGit <109093669+AvishkarsGit@users.noreply.github.com>
2024-05-01 15:19:36 +0000

    dfs using recursion

--- ds_code/tree/graph/bfs.cpp
+++ ds_code/tree/graph/bfs.cpp
@@ -19,21 +19,29 @@ class Edge {
 };
 void createGraph(vector<Edge> graph[]){
 
-    //0-1-2-3  //4-5
-    graph[0].push_back(Edge(0,1));
-
+     graph[0].push_back(Edge(0,1));
+    graph[0].push_back(Edge(0,2));
+    
     graph[1].push_back(Edge(1,0));
-    graph[1].push_back(Edge(1,2));
+    graph[1].push_back(Edge(1,3));
 
-    graph[2].push_back(Edge(2,1));
-    graph[2].push_back(Edge(2,3));
+    graph[2].push_back(Edge(2,0));
+    graph[2].push_back(Edge(2,4));
 
-    graph[3].push_back(Edge(3,2));
+    graph[3].push_back(Edge(3,1));
+    graph[3].push_back(Edge(3,4));
+    graph[3].push_back(Edge(3,5));
 
+    graph[4].push_back(Edge(4,2));
+    graph[4].push_back(Edge(4,3));
     graph[4].push_back(Edge(4,5));
-    
+
+    graph[5].push_back(Edge(5,3));
     graph[5].push_back(Edge(5,4));
-    
+    graph[5].push_back(Edge(5,6));
+
+    graph[6].push_back(Edge(6,5));
+ 
 }
 
 void bfs(vector<Edge> graph[],int v,int start){
@@ -66,14 +74,13 @@ void printGraph(vector<Edge> graph[],int v){
 }
 int main(){
 
-    int v=6;
+    int v=7;
     vector<Edge> graph[v];
     createGraph(graph);
     printGraph(graph,v);
     cout<<endl;
     cout<<"\nBFS:";
     bfs(graph,v,0);
-    bfs(graph,v,4);
     cout<<endl;
     return 0;
 }
--- ds_code/tree/graph/dfs.cpp
+++ ds_code/tree/graph/dfs.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<stack>
 using namespace std;
 class Edge {
     private:
@@ -18,40 +19,61 @@ class Edge {
 };
 void createGraph(vector<Edge> graph[]){
 
-    //0-1-2-3  //4-5
     graph[0].push_back(Edge(0,1));
-
+    graph[0].push_back(Edge(0,2));
+    
     graph[1].push_back(Edge(1,0));
-    graph[1].push_back(Edge(1,2));
+    graph[1].push_back(Edge(1,3));
 
-    graph[2].push_back(Edge(2,1));
-    graph[2].push_back(Edge(2,3));
+    graph[2].push_back(Edge(2,0));
+    graph[2].push_back(Edge(2,4));
 
-    graph[3].push_back(Edge(3,2));
+    graph[3].push_back(Edge(3,1));
+    graph[3].push_back(Edge(3,4));
+    graph[3].push_back(Edge(3,5));
 
+    graph[4].push_back(Edge(4,2));
+    graph[4].push_back(Edge(4,3));
     graph[4].push_back(Edge(4,5));
-    
+
+    graph[5].push_back(Edge(5,3));
     graph[5].push_back(Edge(5,4));
-    
+    graph[5].push_back(Edge(5,6));
+
+    graph[6].push_back(Edge(6,5));
+   
 }
-void dfs(vector<Edge> graph[],int current,vector<bool> visited){
-    cout<<current<<" ";
+void dfs(vector<Edge> graph[],int current,bool visited[]){
+    
     visited[current] = true;
-    for (Edge edge:graph[current]){
-        int i = edge.getDestination();
-        if(!visited[edge.getDestination()]){
-            dfs(graph,edge.getDestination(),visited);
+    cout<<current<<" ";
+    
+    for(Edge edge : graph[current]){
+        if (!visited[edge.getDestination()]){
+            dfs(graph, edge.getDestination(), visited);
         }
     }
 }
+
+void printGraph(vector<Edge> graph[],int v){
+    for(int i = 0;i<v;i++){
+        cout<<"\nvertex "<<i<<" : ";
+        for(Edge edge: graph[i]){
+            cout<<"("<<edge.getSource()<<","<<edge.getDestination()<<")";
+        }
+        cout<<endl;
+    }
+}
+
 int main()
 {
-    int v=6;
+    int v=7;
     vector<Edge> graph[v];
 
     createGraph(graph);
-    vector<bool> visited(v,false);
+    printGraph(graph,v);
+    bool visited[v];
     dfs(graph,0,visited);
-    dfs(graph,4,visited);
+    cout<<endl;
     return 0;
 }
